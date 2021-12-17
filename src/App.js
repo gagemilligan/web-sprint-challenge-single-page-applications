0e615f5c613a053045b3d842863c7d0bf4f2a55c
gagemilligan <gagemilligan03@gmail.com>
2021-12-17 15:46:43 -0500

    codegrade setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import {Link, Switch, Route} from "react-router-dom"
+import * as yup from "yup"
+import Finished from "./components/Finished"
+import Order from "./components/Order"
+import "./App.css";
+
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <p>Your favorite food, delivered while coding.</p>
     </>
   );
 };
